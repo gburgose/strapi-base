53f36f52c041c325b837023cce2ef5cecb44f15c
Gabriel Burgos <hola@gabrielburgos.cl>
2021-01-31 18:54:26 -0300

    chore: update models

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '3016c887e51ff7dc439e7ba866f03545'),
+      secret: env("ADMIN_JWT_SECRET", "3016c887e51ff7dc439e7ba866f03545"),
     },
   },
 });
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'app',
-      script: 'npm',
-      args: 'start',
+      name: "app",
+      script: "npm",
+      args: "start",
     },
   ],
 };
